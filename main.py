d14174ae3bf2882350d061067f1b7f69129fe1a6
Ashish Jadhao <ashishjadhao17@gmail.com>
2021-10-08 11:14:29 +0530

    return headers

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def process_jenkins_event(request):
     # Publish to Pub/Sub
    # publish_to_pubsub(source, body, headers)
     insert_row_into_bigquery(build_event)
-    return build_event
+    return headers
 
 def insert_row_into_bigquery(event):
     if not event:
